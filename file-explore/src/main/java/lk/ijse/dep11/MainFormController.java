09d64ed5fb7eca61f5d2d09ceca68c347f4704e6
Ishanka Dilshan <ishnakadilshan64@gmail.com>
2023-09-19 18:59:40 +0000

    tree view completed

--- file-explore/src/main/java/lk/ijse/dep11/MainFormController.java
+++ file-explore/src/main/java/lk/ijse/dep11/MainFormController.java
@@ -1,4 +1,73 @@
 package lk.ijse.dep11;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.File;
+import java.nio.file.FileStore;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 public class MainFormController {
+    public TreeView treeView;
+    public AnchorPane root;
+    public Button btnNewFolder;
+    public Button btnCopy;
+    public Button btnCut;
+    public Button btnPaste;
+    public Button btnRename;
+    public Button btnNewFile;
+    public ListView lstView;
+    public CheckBox ckboxShowHiddenFiles;
+
+    String property = System.getProperty("user.name");
+    File rootPath = new File("/home/" + property);
+    TreeItem<String> rootTree = new TreeItem<>(rootPath.getName());
+
+
+    public void initialize() {
+        rootTree.getChildren().clear();
+        setTreeView(rootTree, rootPath);
+        treeView.setRoot(rootTree);
+        rootTree.setExpanded(true);
+    }
+
+    public void btnNewFolderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCopyOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCutOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPasteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRenameOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNewFileOnAction(ActionEvent actionEvent) {
+    }
+
+    public void ckboxShowHiddenFilesOnAction(ActionEvent actionEvent) {
+            initialize();
+    }
+
+    public void setTreeView(TreeItem item , File filePath) {
+        File[] filesList = filePath.listFiles();
+        for (File file : filesList) {
+            if (file.getName().charAt(0) == '.' && !ckboxShowHiddenFiles.isSelected()) continue; // hidden files
+//            if (file.isDirectory()) System.out.println(file.getName() + "    Directory");
+//            if (file.isFile()) System.out.println(file.getName() + "   File");
+            TreeItem<String> newItem = new TreeItem<>(file.getName());
+            item.getChildren().add(newItem);
+            if (file.isDirectory()) {
+                setTreeView(newItem , file);
+            }
+        }
+    }
+
 }
---
+++ file-explore/src/main/java/lk/ijse/dep11/SetTreeView.java
@@ -0,0 +1,43 @@
+package lk.ijse.dep11;
+
+import javafx.scene.control.TreeItem;
+
+import java.io.File;
+
+public class SetTreeView {
+
+//    {
+//        System.out.println("bbbbbb");
+//    }
+//
+//    public  void SetTreeView(){
+//        System.out.println("kjkjkkjjkjjkj");
+//        String property = System.getProperty("user.name");
+//        File rootPath = new File("/home/"+property);
+//        System.out.println();
+//        SetTreeView(rootPath, null);
+//    }
+//
+//    public void SetTreeView(File filePath , TreeItem directory){
+//
+//        System.out.println("9999999999999999999999999999999999999");
+//        File[] filesList = filePath.listFiles();
+//
+//        for (File file : filesList) {
+//            if (file.isFile()){
+//                directory.getChildren().add(file.getName());
+//            } else if (file.isDirectory()) {
+//                System.out.println("000000");
+//                TreeItem<String> newDirectory = new TreeItem<>(file.getName());
+//                System.out.println(newDirectory);
+//                MainFormController.treeView.setRoot(newDirectory);
+//                SetTreeView(file , newDirectory);
+//            }else{
+//
+//                System.out.println("I am not");
+//            }
+//        }
+//        return;
+//    }
+
+}
